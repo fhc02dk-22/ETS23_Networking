d43e1b19b63267373e1a5be8dca6e5b17cef3469
dkandlhofer <daniel.kandlhofer@lv.campus02.at>
2023-03-25 09:25:48 +0100

    retrieve info from url

--- src/org/campus02/URLDemo/MyFirstURL.java
+++ src/org/campus02/URLDemo/MyFirstURL.java
@@ -11,7 +11,7 @@ public class MyFirstURL {
         try {
             // Erzeugung der URL
             URL myUrl =
-                    new URL("https://www.campus02.at:443");
+                    new URL("https://www.campus02.at");
 
             // Buffered Reader: Lese die Daten von myUrl
             // Buffered Writer: zum Schreiben
@@ -19,19 +19,32 @@ public class MyFirstURL {
                          new InputStreamReader(myUrl.openStream()));
                  BufferedWriter bw =
                          new BufferedWriter(
-                                 new FileWriter("src/org/campus02/URLDemo/htmlFiles/campus02.html"))
+                                 new FileWriter("src/org/campus02/URLDemo/htmlFiles/campus02.html"));
+                 PrintWriter pw =
+                         new PrintWriter(
+                                 new FileWriter("src/org/campus02/URLDemo/htmlFiles/campus02_pw.html"))
             ) {
                 String line;
                 // lese Zeile für Zeile
                 while ((line = br.readLine()) != null) {
-                    System.out.println(line);
+                    //System.out.println(line);
 
                     // schreibe die Daten in das File
                     bw.write(line);
+                    bw.newLine();
                     //bw.flush();    hier könnten wir zwischenspeichern
+
+                    pw.println(line);
                 }
                 bw.flush(); // !!!! flush nicht vergessen
+                pw.flush();
             }
+
+            System.out.println();
+            System.out.println("myUrl.getProtocol() = " + myUrl.getProtocol());
+            System.out.println("myUrl.getHost() = " + myUrl.getHost());
+            System.out.println("myUrl.getPort() = " + myUrl.getPort());
+            System.out.println("myUrl.getDefaultPort() = " + myUrl.getDefaultPort());
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
