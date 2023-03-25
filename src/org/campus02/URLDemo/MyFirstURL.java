10ec83acebeb22846e26d1d819993c58169af868
dkandlhofer <daniel.kandlhofer@lv.campus02.at>
2023-03-25 09:08:11 +0100

    write to file

--- src/org/campus02/URLDemo/MyFirstURL.java
+++ src/org/campus02/URLDemo/MyFirstURL.java
@@ -1,9 +1,6 @@
 package org.campus02.URLDemo;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -17,14 +14,23 @@ public class MyFirstURL {
                     new URL("https://www.campus02.at:443");
 
             // Buffered Reader: Lese die Daten von myUrl
+            // Buffered Writer: zum Schreiben
             try (BufferedReader br = new BufferedReader(
-                         new InputStreamReader(myUrl.openStream()))) {
-
+                         new InputStreamReader(myUrl.openStream()));
+                 BufferedWriter bw =
+                         new BufferedWriter(
+                                 new FileWriter("src/org/campus02/URLDemo/htmlFiles/campus02.html"))
+            ) {
                 String line;
                 // lese Zeile für Zeile
                 while ((line = br.readLine()) != null) {
                     System.out.println(line);
+
+                    // schreibe die Daten in das File
+                    bw.write(line);
+                    //bw.flush();    hier könnten wir zwischenspeichern
                 }
+                bw.flush(); // !!!! flush nicht vergessen
             }
         } catch (MalformedURLException e) {
             e.printStackTrace();
